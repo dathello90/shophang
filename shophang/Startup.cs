ddbf2c8e61cb92be4b1b46bf116a8dc39f61b49d
quocdat <datseoweb@outlook.com>
2020-11-26 08:58:27 +0700

    Startup dashboard

--- shophang/Startup.cs
+++ shophang/Startup.cs
@@ -59,6 +59,9 @@ namespace shophang
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                   name: "areas",
+                   pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
